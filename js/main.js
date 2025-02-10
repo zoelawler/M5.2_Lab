af1e91897d953dc716f9ec0802ab5a46e553eb01
zoelawler <zoelawler@u.boisestate.edu>
2025-02-10 13:06:29 -0700

    Working on M5.2 lab

--- js/main.js
+++ js/main.js
@@ -1,49 +1,47 @@
-/**
- * This file should only contain functions that don't interact with the DOM.
- * That means no document.querySelector, no document.getElementById, etc.
- * This file should only contain functions that do things like calculations,
- * data manipulation, etc. This is so that we can test these functions
- * without having to worry about the DOM or the browser environment.
- */
+// functionality for showing/hiding the comments section
 
-/**
- * This function prints the string 'Hello World' to the console
- */
-export function helloWorld() {
-    console.log('Hello World');
-}
+const showHideBtn = document.querySelector('.show-hide');
+const commentWrapper = document.querySelector('.comment-wrapper');
 
-/**
- * This function adds two numbers together and returns the sum. This demonstrates
- * how to pass parameters to a function and return a value from a function
- * from HTML.
- * @param {number} a The first number to add
- * @param {number} b The second number to add
- * @returns {number} The sum of the two numbers
- */
-export function add(num1, num2) {
-    if (num1 === undefined || num2 === undefined) {
-        throw new Error('You must provide two numbers to add');
-    }
-    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-        throw new Error('You must provide two numbers to add');
-    }
-    return num1 + num2;
-}
+commentWrapper.style.display = 'none';
 
-/**
- * This function fetches a random joke from the "Official Joke API" and returns it.
- * @returns {string} A joke in the format "setup - punchline"
- */
-export async function fetchRandomJoke() {
-    try {
-        const response = await fetch('https://official-joke-api.appspot.com/random_joke');
-        if (!response.ok) {
-            throw new Error('Failed to fetch a joke');
-        }
-        const joke = await response.json();
-        return `${joke.setup} - ${joke.punchline}`;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+showHideBtn.onclick = function() {
+  let showHideText = showHideBtn.textContent;
+  if(showHideText === 'Show comments') {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+  }
+};
+
+// functionality for adding a new comment via the comments form
+
+const form = document.querySelector('.comment-form');
+const nameField = document.querySelector('#name');
+const commentField = document.querySelector('#comment');
+const list = document.querySelector('.comment-container');
+
+form.onsubmit = function(e) {
+  e.preventDefault();
+  submitComment();
+};
+
+function submitComment() {
+  const listItem = document.createElement('li');
+  const namePara = document.createElement('p');
+  const commentPara = document.createElement('p');
+  const nameValue = nameField.value;
+  const commentValue = commentField.value;
+
+  namePara.textContent = nameValue;
+  commentPara.textContent = commentValue;
+
+  list.appendChild(listItem);
+  listItem.appendChild(namePara);
+  listItem.appendChild(commentPara);
+
+  nameField.value = '';
+  commentField.value = '';
 }
